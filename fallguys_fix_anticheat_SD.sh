0f0276b5c565a33538cf62987e179b152cea2201
DSMan97 <34092854+DSMan97@users.noreply.github.com>
2022-05-12 19:27:28 +0200

    Create fallguys_fix_anticheat_SD.sh

---
+++ fallguys_fix_anticheat_SD.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "Ejecuntando Script para FallGuys en Tarjeta SD..."
+
+STEAMAPPS_PATH="/run/media/mmcblk0p1/steamapps"
+
+FALLGUYS_PATH=$STEAMAPPS_PATH/common/Fall\ Guys
+
+echo "$STEAMAPPS_PATH"
+
+if [ ! -d "$FALLGUYS_PATH" ]; then
+    echo "No se ha econtrado FallGuys en la ubicación seleccionada."
+    exit
+fi
+
+cd "$FALLGUYS_PATH/EasyAntiCheat/"
+rm easyanticheat_x64.so
+cd $HOME
+
+#Create symlink
+ln -s "$FALLGUYS_PATH/EasyAntiCheat/easyanticheat_x64.so" "$FALLGUYS_PATH/FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so"
+
+#Edit ini file
+sed -i.bak 's/TargetApplicationPath=.*/TargetApplicationPath=FallGuys_client_game.exe/' "$FALLGUYS_PATH/FallGuys_client.ini"
+
+#Try to install Proton EAC Runtime
+if which xdg-open > /dev/null
+then
+  xdg-open steam://install/1826330 & > /dev/null
+elif which gnome-open > /dev/null
+then
+  gnome-open steam://install/1826330 & > /dev/null
+fi
+
+exit
